d84d71aeac85ccd630958a3ba465ca57c11fb649
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-09-26 14:31:30 +0100

    Update launch_game.py

--- launch_game.py
+++ launch_game.py
@@ -5,15 +5,24 @@ from datetime import datetime
 
 
 
-def toggle_screen(toggle):
-    subprocess.call(["vcgencmd", "display_power", toggle])
+#def toggle_screen(toggle):
+#    subprocess.call(["vcgencmd", "display_power", toggle])
 
-time.sleep(2)
+#time.sleep(2)
 
-subprocess.call(["/opt/retropie/supplementary/runcommand/runcommand.sh" ,"_SYS_", "snes", "'/home/pi/RetroPie/roms/snes/test.smc'"])
-#
+subprocess.call(["/opt/retropie/supplementary/runcommand/runcommand.sh", "0","_SYS_", "snes", "'/home/pi/RetroPie/roms/snes/test.smc'"])
 
-time.sleep(15)
-toggle_screen("0")
-time.sleep(1)
-toggle_screen("1")
+
+# We configure sudo nano /opt/retropie/configs/all/autostart.sh
+#/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ snes '/home/pi/RetroPie/roms/snes/test.smc'
+# sudo nano /opt/retropie/configs/all/runcommand.cfg
+# Then set disable_menu = "1"
+
+
+#time.sleep(15)
+#toggle_screen("0")
+#time.sleep(1)
+#toggle_screen("1")
+
+
+#/opt/retropie/configs/all $ sudo nano runcommand.cfg
