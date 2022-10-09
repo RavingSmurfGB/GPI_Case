9da720064a7f662064947611e4b3fe495f21d66e
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-09 20:39:14 +0100

    Update client.py

--- client.py
+++ client.py
@@ -9,13 +9,15 @@ with open("save_state.txt") as file: # Use file to refer to the file object
 
    'if data = normtal_boot'
     # Launch normal boot
-    'elif data = lock_boot
+    'elif data = lock_boot'
     #Launch game and change hotkey in /opt/retropie/configs/all/retroarch/autoconfig To ensure user cannot exit out of game
 
 
 
 
 while True: # Await code input
+    key_combo = []
+    event_list = []
     events = get_gamepad()
     for event in events:
         #if event equals code
