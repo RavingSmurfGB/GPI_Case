f066837a2860af12202ac06f709611c297af9fa0
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-09 22:02:11 +0100

    Update client.py

--- client.py
+++ client.py
@@ -10,14 +10,14 @@ with open("save_state.txt") as file: # Use file to refer to the file object
 
     if data == "normal_boot":
         ' launch emulation station shell script and hang'
-        os.system("./launch_game.sh")
+        os.system("./home/pi/GPI_Case/launch_game.sh")
         while True:
             time.sleep(1)
         pass
 
 
     elif data == "lock_boot":
-        os.system("./launch_emulationstation.sh")
+        os.system("./home/pi/GPI_Case/launch_emulationstation.sh")
         'launch shell script with the following and proceed'
         # Also change controlls - but controls only reload with reboot....
         #/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ snes '/home/pi/RetroPie/roms/snes/The Legend of Zelda - A Link to the Past (U) [!].smc'
