09bce65e0a672143aa707eee3a702c2148e6179b
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-09 22:15:02 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import os, time
 
 
 #Get Save State 
-with open("save_state.txt") as file: # Use file to refer to the file object
+with open("/home/pi/GPI_Case/save_state.txt") as file: # Use file to refer to the file object
 
     data = file.read()
 
