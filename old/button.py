99ed60150a3f5632912b8d23832ec2dbd6b181d7
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-11 23:37:51 +0100

    Update button.py

--- old/button.py
+++ old/button.py
@@ -5,13 +5,14 @@ from inputs import get_gamepad
 def write_file(data, mode):
 
         with open("button_output.txt", mode) as file:
-                file.write(str(data)) # the data is writen to file
+                file.write(str(data) +  "\n") # the data is writen to file
+                #file.write( "\n")
 
 write_file("", "w")
 
 while True:
     events = get_gamepad()
     for event in events: #
-        event_list = [event.ev_type, event.code, event.state, "\n"]
+        event_list = [event.ev_type, event.code, event.state]
         print(event_list)
         write_file(event_list, "a")
