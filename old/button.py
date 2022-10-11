ee27ea841054ac3c3f2f863ae500cabf66ad746b
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-11 23:33:56 +0100

    Update button.py

--- old/button.py
+++ old/button.py
@@ -12,6 +12,6 @@ write_file("", "w")
 while True:
     events = get_gamepad()
     for event in events: #
-        event_list = [event.ev_type, event.code, event.state]
+        event_list = [event.ev_type, event.code, event.state, "\n"]
         print(event_list)
         write_file(event_list, "a")
