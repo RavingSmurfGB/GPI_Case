2b96119814f4b2aca7972c6e59c93564ed3ee5bc
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-11 23:28:09 +0100

    Update button.py

--- old/button.py
+++ old/button.py
@@ -8,8 +8,8 @@ def write_file(data):
         with open("button_output.txt", "w") as file:
                 file.write(data) # the data is writen to file
 
-        with open("button_output.txt", "x") as file:
-            file.write()
+        #with open("button_output.txt", "x") as file:
+         #   file.write()
 
 
 events = get_gamepad()
