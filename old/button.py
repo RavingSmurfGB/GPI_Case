27fc294f010b854fdce67c7103099b234602f63b
RavingSmurfGB <josephhutchinson4@gmail.com>
2022-10-11 23:39:10 +0100

    Update button.py

--- old/button.py
+++ old/button.py
@@ -6,13 +6,15 @@ def write_file(data, mode):
 
         with open("button_output.txt", mode) as file:
                 file.write(str(data) +  "\n") # the data is writen to file
-                #file.write( "\n")
+
 
 write_file("", "w")
 
 while True:
     events = get_gamepad()
     for event in events: #
-        event_list = [event.ev_type, event.code, event.state]
-        print(event_list)
-        write_file(event_list, "a")
+        if event.ev_type == "Absolute":
+            event_list = [event.ev_type, event.code, event.state]
+
+            print(event_list)
+            write_file(event_list, "a")
